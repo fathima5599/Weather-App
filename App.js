738935a88bd6eebf411793e658144e957776216b
Fathima Mary.M <163809708+fathima5599@users.noreply.github.com>
2024-07-23 22:43:15 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './App.css';
+//import Welcome from './components/WelcomeFunctionalComponents';
+//import Welcome from './components/nctionalComponents';
+//import Information from'./components/Information';
+//import PostData from './components/PostData';
+//import PostDataFromForm from './components/PostDataFromForm';
+//import HooksCounterFunctionalCom from './components/HooksCounterFunctionalCom';
+
+//import ComponentA from './components/ComponentA';
+
+//export const UserContext=React.createContext();
+
+//import StudentRegistrationForm from './StudentRegistrationForm';
+
+import Weather from './components/Weather';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/*<UserContext.Provider value={'Mani'}>
+        <ComponentA/>
+      </UserContext.Provider>
+     {/*<Welcome/>*/}
+     {/*<Greeating name="Ganesh" spotrsNAme="FootBall">
+      <p>Lorem ipsum dolor sir amet,consectetur</p>
+      </Greeating>
+      <Greeating name="magesh" sportsName="koho">
+        <button>Click Me</button>
+    </Greeating>*/}
+     {/*<Information/>*/}
+
+
+  
+     {/*<PostData/>*/}
+
+     {/*<PostDataFromForm/>*/}
+     {/*<HooksCounterFunctionalCom/>*/}
+
+
+     {/*<StudentRegistrationForm />*/}
+
+     <Weather />
+
+
+     
+    </div>
+  );
+
+  
+}
+
+export default App;
